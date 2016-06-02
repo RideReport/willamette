a928678544bbb497431dae360fb4651feb638c83
Evan Heidtmann <evan.heidtmann@gmail.com>
2016-06-02 11:56:44 -0700

    API history

--- poc.py
+++ poc.py
@@ -36,7 +36,7 @@ def getrows(dt=None):
         dt = dateutil.parser.parse(date)
         row['dt'] = dt
         yield row
-    
+
 def get_most_recent_dt_and_value(rows, key):
     row = max([r for r in rows if r.get(key, '') != ''], key=lambda r: r['dt'])
     return row['dt'], row[key]
@@ -80,14 +80,23 @@ def gen_bes_ecoli(url, rank=0):
         }
 
 def get_nearby_algae_bloom_advisories():
+    if datetime.now().year != 2016:
+        return [{
+            'issued_description': 'Unknown! API needs to be updated for curent year.',
+        }]
+
     with open(os.path.join(SCRIPT_DIR, 'willamette-zone.wkt')) as f:
         poly_wkt = f.read()
-    url = 'https://data.oregon.gov/resource/hbyw-ngxj.json'
+    url = 'https://data.oregon.gov/resource/muwp-rr44.json'
     query = {
         '$where': 'within_polygon(location, "{}")'.format(poly_wkt),
     }
     response = requests.get(url, params=query)
-    response.raise_for_status()
+    try:
+        response.raise_for_status()
+    except:
+        print response.text
+        raise
     results = response.json()
     return results
 
@@ -96,21 +105,76 @@ morrison = list(gen_bes_ecoli('http://www.portlandoregon.gov/bes/waterquality/re
 marina = list(gen_bes_ecoli('http://www.portlandoregon.gov/bes/waterquality/results.cfm?location_id=7132', 0))
 
 from itertools import chain
-ecoli = max(chain(boathouse, morrison, marina), key=lambda r: (r['dt'], r['rank']))
+ecoli_history = sorted(chain(boathouse, morrison, marina), key=lambda r: (r['dt'], r['rank']), reverse=True)
+ecoli = ecoli_history[0]
 
-TCdt, TC = get_most_recent_dt_and_value(rows, '01_00010')
-tbdt, tb = get_most_recent_dt_and_value(rows, '38_63680')
-cydt, cy = get_most_recent_dt_and_value(rows, '52_95204')
+
+k_TC = '01_00010'
+k_tb = '38_63680'
+k_cy = '52_95204'
+TCdt, TC = get_most_recent_dt_and_value(rows, k_TC)
+tbdt, tb = get_most_recent_dt_and_value(rows, k_tb)
+cydt, cy = get_most_recent_dt_and_value(rows, k_cy)
 
 data = {
-    'temperature_celcius': float(TC),
+    'temperature_celsius': float(TC),
     'temperature_date': str(TCdt),
-    'turbidity': float(tb),
-    'turbidity_date': str(tbdt),
+    #'turbidity': float(tb),
+    #'turbidity_date': str(tbdt),
     'ecoli': float(ecoli['ecoli']),
     'ecoli_date': str(ecoli['dt']),
-    'cyanobacteria': float(cy),
-    'cyanobacteria_date': str(cydt),
+    #'cyanobacteria': float(cy),
+    #'cyanobacteria_date': str(cydt),
+    'algae_bloom_advisories': get_nearby_algae_bloom_advisories(),
+
+    'history': {
+        'usgs_temperature':
+            [
+                {
+                    'celsius': float(r[k_TC]),
+                    'fahrenheit': 9./5. * float(r[k_TC]) + 32.,
+                    'date': str(r['dt'])
+                }
+                for r in rows if r.get(k_TC, '') != ''
+            ],
+        #'usgs_turbidity':
+        #    [
+        #        {
+        #            'turbidity': float(r[k_tb]),
+        #            'date': str(r['dt']),
+        #        }
+        #        for r in rows if r.get(k_tb, '') != ''
+        #    ],
+        'bes_ecoli':
+            [
+                {
+                    'date': str(r['dt']),
+                    'temperature_fahrenheit': r['tempf'],
+                    'organisms_per_100ml': r['ecoli'],
+                    'weight': -r['rank'], # relevance to hawthorne bridge dock. lower numbers more relevant.
+                    'location_name': r['name'],
+                }
+                for r in ecoli_history
+            ],
+    },
+
+    'human_readable_source_links': [
+        {
+            'agency': 'USGS',
+            'description': 'National Water Information System, Willamette River station',
+            'url': 'http://waterdata.usgs.gov/nwis/uv?cb_00010=on&cb_00060=on&cb_00065=on&cb_00095=on&cb_00055=on&cb_00300=on&cb_00400=on&cb_63680=on&cb_62361=on&cb_95204=on&cb_99137=on&cb_32295=on&format=html&site_no=14211720&period=7',
+        },
+        {
+            'agency': 'City of Portland, BES',
+            'description': 'Environmental Services collects water samples from the river monthly at three locations to analyze long-term water quality trends for nutrients, metals and bacteria. From May to October, field staff also collect weekly samples from five  Willamette River public access points to give recreational users more frequent information about bacteria levels.',
+            'url': 'https://www.portlandoregon.gov/bes/waterquality/results.cfm',
+        },
+        {
+            'agency': 'Oregon Health Authority',
+            'description': 'Blue-green algae advisories for the state of Oregon',
+            'url': 'https://public.health.oregon.gov/HealthyEnvironments/Recreation/HarmfulAlgaeBlooms/Pages/Blue-GreenAlgaeAdvisories.aspx',
+        },
+    ]
 }
 
 with open('data.json', 'w') as f:

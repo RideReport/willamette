4a87ec4def91499c705d08a8e753ec47ba0a8a7c
Evan Heidtmann <evan.heidtmann@gmail.com>
2017-05-10 16:22:12 -0700

    Adapt for lambda, with makefile to build zip

--- poc.py
+++ poc.py
@@ -7,6 +7,7 @@ import pytz
 import boto
 import json
 import os
+from itertools import chain
 pacific = pytz.timezone('US/Pacific')
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -40,9 +41,6 @@ def getrows(dt=None):
 def get_most_recent_dt_and_value(rows, key):
     row = max([r for r in rows if r.get(key, '') != ''], key=lambda r: r['dt'])
     return row['dt'], row[key]
-
-rows = list(getrows())
-
 
 def gen_bes_ecoli(url, rank=0):
     response = requests.get(url)
@@ -96,87 +94,103 @@ def get_nearby_algae_bloom_advisories():
     results = response.json()
     return results
 
-boathouse = list(gen_bes_ecoli('http://www.portlandoregon.gov/bes/waterquality/results.cfm?location_id=7131', 2))
-morrison = list(gen_bes_ecoli('http://www.portlandoregon.gov/bes/waterquality/results.cfm?location_id=1727', 1))
-marina = list(gen_bes_ecoli('http://www.portlandoregon.gov/bes/waterquality/results.cfm?location_id=7132', 0))
+def lambda_handler(event, context):
+    data = get_data_and_push_to_s3()
+    text = json.dumps(data, indent=2)
+    return { "detail": "Updated s3 with {} chars".format(len(text)), "data": data }
 
-from itertools import chain
-ecoli_history = sorted(chain(boathouse, morrison, marina), key=lambda r: (r['dt'], r['rank']), reverse=True)
-ecoli = ecoli_history[0]
+def get_data_and_push_to_s3():
+    data = get_data()
+    upload_to_s3(data)
+    return data
+
+def get_data():
+    rows = list(getrows())
+
+    boathouse = list(gen_bes_ecoli('http://www.portlandoregon.gov/bes/waterquality/results.cfm?location_id=7131', 2))
+    morrison = list(gen_bes_ecoli('http://www.portlandoregon.gov/bes/waterquality/results.cfm?location_id=1727', 1))
+    marina = list(gen_bes_ecoli('http://www.portlandoregon.gov/bes/waterquality/results.cfm?location_id=7132', 0))
+
+    ecoli_history = sorted(chain(boathouse, morrison, marina), key=lambda r: (r['dt'], r['rank']), reverse=True)
+    ecoli = ecoli_history[0]
 
 
-k_TC = '172755_00010'
-k_tb = '172757_63680'
-k_cy = '173554_95204'
-TCdt, TC = get_most_recent_dt_and_value(rows, k_TC)
-tbdt, tb = get_most_recent_dt_and_value(rows, k_tb)
-cydt, cy = get_most_recent_dt_and_value(rows, k_cy)
+    k_TC = '172755_00010'
+    k_tb = '172757_63680'
+    k_cy = '173554_95204'
+    TCdt, TC = get_most_recent_dt_and_value(rows, k_TC)
+    tbdt, tb = get_most_recent_dt_and_value(rows, k_tb)
+    cydt, cy = get_most_recent_dt_and_value(rows, k_cy)
 
-data = {
-    'temperature_celsius': float(TC),
-    'temperature_date': str(TCdt),
-    #'turbidity': float(tb),
-    #'turbidity_date': str(tbdt),
-    'ecoli': float(ecoli['ecoli']),
-    'ecoli_date': str(ecoli['dt']),
-    #'cyanobacteria': float(cy),
-    #'cyanobacteria_date': str(cydt),
-    'algae_bloom_advisories': get_nearby_algae_bloom_advisories(),
-
-    'history': {
-        'usgs_temperature':
-            [
-                {
-                    'celsius': float(r[k_TC]),
-                    'fahrenheit': 9./5. * float(r[k_TC]) + 32.,
-                    'date': str(r['dt'])
-                }
-                for r in rows if r.get(k_TC, '') != ''
-            ],
-        #'usgs_turbidity':
-        #    [
-        #        {
-        #            'turbidity': float(r[k_tb]),
-        #            'date': str(r['dt']),
-        #        }
-        #        for r in rows if r.get(k_tb, '') != ''
-        #    ],
-        'bes_ecoli':
-            [
-                {
-                    'date': str(r['dt']),
-                    'temperature_fahrenheit': r['tempf'],
-                    'organisms_per_100ml': r['ecoli'],
-                    'weight': -r['rank'], # relevance to hawthorne bridge dock. lower numbers more relevant.
-                    'location_name': r['name'],
-                }
-                for r in ecoli_history
-            ],
-    },
-
-    'human_readable_source_links': [
-        {
-            'agency': 'USGS',
-            'description': 'National Water Information System, Willamette River station',
-            'url': 'http://waterdata.usgs.gov/nwis/uv?cb_00010=on&cb_00060=on&cb_00065=on&cb_00095=on&cb_00055=on&cb_00300=on&cb_00400=on&cb_63680=on&cb_62361=on&cb_95204=on&cb_99137=on&cb_32295=on&format=html&site_no=14211720&period=7',
+    data = {
+        'temperature_celsius': float(TC),
+        'temperature_date': str(TCdt),
+        #'turbidity': float(tb),
+        #'turbidity_date': str(tbdt),
+        'ecoli': float(ecoli['ecoli']),
+        'ecoli_date': str(ecoli['dt']),
+        #'cyanobacteria': float(cy),
+        #'cyanobacteria_date': str(cydt),
+        'algae_bloom_advisories': get_nearby_algae_bloom_advisories(),
+        'generated_date': str(pytz.utc.localize(datetime.utcnow())),
+        'history': {
+            'usgs_temperature':
+                [
+                    {
+                        'celsius': float(r[k_TC]),
+                        'fahrenheit': 9./5. * float(r[k_TC]) + 32.,
+                        'date': str(r['dt'])
+                    }
+                    for r in rows if r.get(k_TC, '') != ''
+                ],
+            #'usgs_turbidity':
+            #    [
+            #        {
+            #            'turbidity': float(r[k_tb]),
+            #            'date': str(r['dt']),
+            #        }
+            #        for r in rows if r.get(k_tb, '') != ''
+            #    ],
+            'bes_ecoli':
+                [
+                    {
+                        'date': str(r['dt']),
+                        'temperature_fahrenheit': r['tempf'],
+                        'organisms_per_100ml': r['ecoli'],
+                        'weight': -r['rank'], # relevance to hawthorne bridge dock. lower numbers more relevant.
+                        'location_name': r['name'],
+                    }
+                    for r in ecoli_history
+                ],
         },
-        {
-            'agency': 'City of Portland, BES',
-            'description': 'Environmental Services collects water samples from the river monthly at three locations to analyze long-term water quality trends for nutrients, metals and bacteria. From May to October, field staff also collect weekly samples from five  Willamette River public access points to give recreational users more frequent information about bacteria levels.',
-            'url': 'https://www.portlandoregon.gov/bes/waterquality/results.cfm',
-        },
-        {
-            'agency': 'Oregon Health Authority',
-            'description': 'Blue-green algae advisories for the state of Oregon',
-            'url': 'https://public.health.oregon.gov/HealthyEnvironments/Recreation/HarmfulAlgaeBlooms/Pages/Blue-GreenAlgaeAdvisories.aspx',
-        },
-    ]
-}
 
-with open('data.json', 'w') as f:
-    json.dump(data, f)
+        'human_readable_source_links': [
+            {
+                'agency': 'USGS',
+                'description': 'National Water Information System, Willamette River station',
+                'url': 'http://waterdata.usgs.gov/nwis/uv?cb_00010=on&cb_00060=on&cb_00065=on&cb_00095=on&cb_00055=on&cb_00300=on&cb_00400=on&cb_63680=on&cb_62361=on&cb_95204=on&cb_99137=on&cb_32295=on&format=html&site_no=14211720&period=7',
+            },
+            {
+                'agency': 'City of Portland, BES',
+                'description': 'Environmental Services collects water samples from the river monthly at three locations to analyze long-term water quality trends for nutrients, metals and bacteria. From May to October, field staff also collect weekly samples from five  Willamette River public access points to give recreational users more frequent information about bacteria levels.',
+                'url': 'https://www.portlandoregon.gov/bes/waterquality/results.cfm',
+            },
+            {
+                'agency': 'Oregon Health Authority',
+                'description': 'Blue-green algae advisories for the state of Oregon',
+                'url': 'https://public.health.oregon.gov/HealthyEnvironments/Recreation/HarmfulAlgaeBlooms/Pages/Blue-GreenAlgaeAdvisories.aspx',
+            },
+        ]
+    }
 
-s3 = boto.connect_s3()
-key = s3.get_bucket('howsthewater').new_key('data.json')
-key.set_contents_from_filename('data.json')
-key.set_acl('public-read')
+    return data
+
+def upload_to_s3(data):
+    s3 = boto.connect_s3()
+    key = s3.get_bucket('howsthewater').new_key('data.json')
+    key.set_contents_from_string(json.dumps(data, indent=2))
+    key.set_acl('public-read')
+
+if __name__ == '__main__':
+    data = get_data()
+    print json.dumps(data, indent=2)
